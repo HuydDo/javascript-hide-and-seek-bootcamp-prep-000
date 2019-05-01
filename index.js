ceb93b837f3858a78d6d2b2123af2c1c3096da68
Huy do <huyddo40@gmail.com>
2019-05-01 02:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector (selector){
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget(){
+   return document.querySelector('div.target')
+}
+
+function deepestChild(){
+  let div = document.getElementById('grand-node');
+  let nextdiv = div.children[0];
+  while (nextdiv){
+    div = nextdiv;
+    nextdiv = div.children[0];
+  }
+  return div;
+}
+
+function increaseRankBy(n){
+  const div = document.querySelectorAll('.rankedlist')
+  for (let i = 0; i< div.length; i++){
+   div[i].innerHTML = parseInt(i + n);
+  }
+}
